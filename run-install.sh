1d98b4b6e4cc3a86fc20c9efa46cace141522991
PostalPanda <easton+git@postalpanda.com>
2023-10-29 10:03:52 -0700

    Fixed installer hang
    Fixed the installer hang when the python modules were already installed.

--- run-install.sh
+++ run-install.sh
@@ -8,24 +8,48 @@ log_message() {
     echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
 }
 
-# Install the 'keyboard' and 'pyfiglet' package using pip3 and log the step
-log_message "Installing the 'keyboard' package using pip3..."
-pip3 install keyboard pyfiglet >> "$LOG_FILE" 2>&1
-
-# Check if the installation was successful
-if [ $? -eq 0 ]; then
-    log_message "keyboard package installed successfully."
-    log_message "Running install.py..."
-
-    # Run your install.py script and log the step
-    python3 install.py >> "$LOG_FILE" 2>&1
-
-    # Check the exit status of install.py
-    if [ $? -eq 0 ]; then
-        log_message "install.py completed successfully."
-    else
-        log_message "Error: install.py exited with a non-zero status."
-    fi
+# Check if the 'keyboard' package is already installed
+if pip3 show keyboard > /dev/null 2>&1; then
+    log_message "'keyboard' package is already installed."
 else
-    log_message "Error: Failed to install the 'keyboard' package."
+    # Install the 'keyboard' package using pip3 and log the step
+    log_message "Installing the 'keyboard' package using pip3..."
+    pip3 install keyboard >> "$LOG_FILE" 2>&1
+
+    # Check if the installation was successful
+    if [ $? -eq 0 ]; then
+        log_message "keyboard package installed successfully."
+    else
+        log_message "Error: Failed to install the 'keyboard' package."
+        exit 1
+    fi
+fi
+
+# Check if the 'pyfiglet' package is already installed
+if pip3 show pyfiglet > /dev/null 2>&1; then
+    log_message "'pyfiglet' package is already installed."
+else
+    # Install the 'pyfiglet' package using pip3 and log the step
+    log_message "Installing the 'pyfiglet' package using pip3..."
+    pip3 install pyfiglet >> "$LOG_FILE" 2>&1
+
+    # Check if the installation was successful
+    if [ $? -eq 0 ]; then
+        log_message "pyfiglet package installed successfully."
+    else
+        log_message "Error: Failed to install the 'pyfiglet' package."
+        exit 1
+    fi
+fi
+
+log_message "Running install.py..."
+
+# Run your install.py script and log the step
+python3 install.py >> "$LOG_FILE" 2>&1
+
+# Check the exit status of install.py
+if [ $? -eq 0 ]; then
+    log_message "install.py completed successfully."
+else
+    log_message "Error: install.py exited with a non-zero status."
 fi
